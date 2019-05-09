3d0f158b57e98753eac8b0a0fe4bf637debffbc5
Paul Okunev <paul.okunev00@gmail.com>
2019-05-09 22:00:39 +0300

    update py files

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,3 @@
+
+        leng = len(chromosome1.get_genes())
+        idx = random.randint(0,leng) #generate index of crossover
---
+++ test.py
@@ -0,0 +1,27 @@
+import math
+import random
+list1 = [1,2,4,6,8,10]
+#pair = random.sample(list1,2)
+
+#list1 = [x for x in list1 if x not in pair]
+#print(pair)
+#print(pair[0],pair[1])
+#list2 = ["a","b","c"]
+#list1.extend(list2)
+#print(list1)
+
+
+#print(deconvert([1,0,1,1]))
+#list1 += [11,12]
+#print(list1)
+#def selection(pop):
+#    interList = []
+#    breeding = 0
+#    avgFitness = (0 if not pop else sum(pop) / len(pop))
+    #randval     
+#    return avgFitness
+
+#print(selection(list1))
+#print((5/3) // 1.0)
+#print(5/3)
+
